3bb707d0a450ec08ee372b90e3045eb18bc3deaf
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-23 14:21:53 -0700

    Adds Item validation tests and integrations.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,7 +2,7 @@ class InvoiceItem < ApplicationRecord
   validates_presence_of :item_id, :invoice_id, :quantity, :unit_price
 
   validates :quantity, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  validates :unit_price, numericality: {greater_than: 0}
+  validates :unit_price, numericality: {only_integer: true, greater_than: 0}
 
   # belongs_to :item
   # belongs_to :invoice
