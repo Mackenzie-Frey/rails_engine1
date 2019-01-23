ea8cb360c2475a1607ad9854836cb0118ffb9c14
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-23 14:14:45 -0700

    Adds invoice and invoice item validation tests and integrations.

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
   describe 'Validations' do
-    it {should validate_presence_of }
+    it {should validate_presence_of :item_id}
+    it {should validate_presence_of :invoice_id}
+    it {should validate_presence_of :quantity}
+    it {should validate_presence_of :unit_price}
   end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it {should validate_presence_of :customer_id}
+    it {should validate_presence_of :merchant_id}
+    it {should validate_presence_of :status}
+  end
 end
