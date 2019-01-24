83c9e1a2c848c8b9a0972ef89360c89f85bb423d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-24 14:56:59 -0700

    Removes controller tests (generated via use of scaffolding)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :transactions, only: :show
-      resources :merchants, only: :show
-      resources :items, only: :show
-      resources :invoices, only: :show
-      resources :invoice_items, only: :show
-      resources :customers, only: :show
+      resources :transactions, only: [:show]
+      resources :merchants, only: [:show]
+      resources :items, only: [:show]
+      resources :invoices, only: [:show]
+      resources :invoice_items, only: [:show]
+      resources :customers, only: [:show]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
