9d5ceea9f11457312ce978269a737131db723e8d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-24 07:32:19 -0700

    Top_Merchants method in progress

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,19 @@ class Customer < ApplicationRecord
   has_many :invoices
 
   def favorite_merchant
+    # Merchant
+    # # .select("merchants.* FROM merchants")
+    # # .joins("invoices ON invoices.merchant_id = merchants.id")
+    # .joins(:transactions)
+    # .where("merchant_id")
+    #
+    # .order("transaction_count DESC")
+    # .limit(1)
+
+  end
+
+  def self.top_merchants
+    binding.pry
+    # Merchant.joins(invoices: :transactions)
   end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -10,17 +10,30 @@ RSpec.describe Customer, type: :model do
     it {should have_many :invoices}
   end
 
+  describe 'Class Methods' do
+    before :each do
+      @merchant_1 = create(:merchant)
+      @merchant_2 = create(:merchant)
+      @merchant_3 = create(:merchant)
+
+      it '.top_merchants' do
+
+
+        expect(Merchant.top_merchants).to eq([@merchant_3, @merchant_2, @merchant_1])
+      end
+    end
+  end
+
   describe 'Instance Methods' do
-    it '.favorite_merchant' do
+    xit '.favorite_merchant' do
       customer = create(:customer)
 
       merchant_1 = create(:merchant)
       merchant_2 = create(:merchant)
 
-      invoice_1 = create(:invoice, customer: customer, merchant: merchant_1)
+      invoice_1 = create(:invoice, customer: customerinvoices, merchant: merchant_1)
       invoice_2 = create(:invoice, customer: customer, merchant: merchant_1)
       invoice_3 = create(:invoice, customer: customer, merchant: merchant_2)
-
 
       success_transaction_1 = create(:success_transaction, invoice: invoice_1)
       success_transaction_2 = create(:success_transaction, invoice: invoice_2)
