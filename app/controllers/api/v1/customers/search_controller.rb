3bc5f4f42f80440853e00ecce8e393927d547345
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-26 15:21:32 -0700

    Passes first_name single finder for customer

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::Customers::SearchController < ApplicationController
   def show
     render json: CustomerSerializer.new(Customer.find_by(look_up_params))
+    #right about after .find_by, look up maybe sql to pul in there
   end
 
   private
--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -28,7 +28,39 @@ describe "Customer API - Single Finders" do
     expect(customers["data"]["attributes"]["first_name"]).to eq("#{c1.first_name}")
     expect(customers["data"]["attributes"]["last_name"]).to eq("#{c1.last_name}")
   end
+
+  it 'Single finders - first_name' do
+    c1 = create(:customer, first_name: "Joey", last_name: "Ondricka", created_at: "2012-03-27 14:54:09 UTC", updated_at: "2012-03-27 14:54:09 UTC")
+    c2 = create(:customer, first_name: "Name_2", last_name: "Last Name", created_at: "2012-04-27 14:54:09 UTC", updated_at: "2012-04-27 14:54:09 UTC")
+
+    get "/api/v1/customers/find?first_name=#{c1.first_name}"
+
+    expect(response).to be_successful
+
+    customers = JSON.parse(response.body)
+
+    expect(customers["data"]["id"]).to eq("#{c1.id}")
+    expect(customers["data"]["attributes"]["first_name"]).to eq("#{c1.first_name}")
+    expect(customers["data"]["attributes"]["last_name"]).to eq("#{c1.last_name}")
+  end
+
+  it 'Single finders - last_name' do
+    c1 = create(:customer, first_name: "Joey", last_name: "Ondricka", created_at: "2012-03-27 14:54:09 UTC", updated_at: "2012-03-27 14:54:09 UTC")
+    c2 = create(:customer, first_name: "Name_2", last_name: "Last Name", created_at: "2012-04-27 14:54:09 UTC", updated_at: "2012-04-27 14:54:09 UTC")
+
+    get "/api/v1/customers/find?last_name=#{c1.last_name}"
+
+    expect(response).to be_successful
+
+    customers = JSON.parse(response.body)
+
+    expect(customers["data"]["id"]).to eq("#{c1.id}")
+    expect(customers["data"]["attributes"]["first_name"]).to eq("#{c1.first_name}")
+    expect(customers["data"]["attributes"]["last_name"]).to eq("#{c1.last_name}")
+  end
 end
+
+#   id,first_name,last_name,created_at,updated_at
 
 # do for customers first test, routing, serializing. hand roll routes (rewrite
 # index, show, first name , last name, create_at, updated at
