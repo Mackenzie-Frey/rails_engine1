0787e7038cd8a6fd4283acf929799786416a8582
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-26 16:58:19 -0700

    Adds tests and integration for multi-finders for merchants. Adds route to accomodate. Passes tests.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -28,4 +28,117 @@ describe "Merchant API - Single Finders" do
     expect(merchants["data"]["id"]).to eq("#{m1.id}")
     expect(merchants["data"]["attributes"]["name"]).to eq("#{m1.name}")
   end
+
+  it 'Single finders - Name' do
+    m1 = create(:merchant, name: "M1", created_at: "2012-03-27 14:54:09 UTC", updated_at: "2012-03-27 14:54:09 UTC")
+    m2 = create(:merchant, name: "M2", created_at: "2012-04-27 14:54:09 UTC", updated_at: "2012-04-27 14:54:09 UTC")
+
+    get "/api/v1/merchants/find?name=#{m1.name}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants["data"]["id"]).to eq("#{m1.id}")
+    expect(merchants["data"]["attributes"]["name"]).to eq("#{m1.name}")
+  end
+
+  it 'Single finders - Created At' do
+    m1 = create(:merchant, name: "M1", created_at: "2012-03-27 14:54:09 UTC", updated_at: "2012-03-27 14:54:09 UTC")
+    m2 = create(:merchant, name: "M2", created_at: "2012-04-27 14:54:09 UTC", updated_at: "2012-04-27 14:54:09 UTC")
+
+    get "/api/v1/merchants/find?created_at=#{m1.created_at}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants["data"]["id"]).to eq("#{m1.id}")
+    expect(merchants["data"]["attributes"]["name"]).to eq("#{m1.name}")
+  end
+
+  it 'Single finders - Updated At' do
+    m1 = create(:merchant, name: "M1", created_at: "2012-03-27 14:54:09 UTC", updated_at: "2012-03-27 14:54:09 UTC")
+    m2 = create(:merchant, name: "M2", created_at: "2012-04-27 14:54:09 UTC", updated_at: "2012-04-27 14:54:09 UTC")
+
+    get "/api/v1/merchants/find?updated_at=#{m1.updated_at}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants["data"]["id"]).to eq("#{m1.id}")
+    expect(merchants["data"]["attributes"]["name"]).to eq("#{m1.name}")
+  end
+end
+
+describe "Customer API - Multi-Finders" do
+  it 'Multi-finders - ID' do
+    m1 = create(:merchant, name: "M1", created_at: "2012-03-27 14:54:09 UTC", updated_at: "2012-03-27 14:54:09 UTC")
+    m2 = create(:merchant, name: "M2", created_at: "2012-04-27 14:54:09 UTC", updated_at: "2012-04-27 14:54:09 UTC")
+
+    get "/api/v1/merchants/find_all?id=#{m1.id}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants["data"][0]["id"]).to eq("#{m1.id}")
+    expect(merchants["data"][0]["attributes"]["name"]).to eq("#{m1.name}")
+  end
+
+  it 'Multi finders - Name' do
+    m1 = create(:merchant, name: "M", created_at: "2012-03-27 14:54:09 UTC", updated_at: "2012-03-27 14:54:09 UTC")
+    m2 = create(:merchant, name: "M", created_at: "2012-04-27 14:54:09 UTC", updated_at: "2012-04-27 14:54:09 UTC")
+    m3 = create(:merchant, name: "M3", created_at: "2012-04-27 14:54:09 UTC", updated_at: "2012-04-27 14:54:09 UTC")
+
+    get "/api/v1/merchants/find_all?name=#{m1.name}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants["data"].count).to eq(2)
+    expect(merchants["data"][0]["id"]).to eq("#{m1.id}")
+    expect(merchants["data"][1]["id"]).to eq("#{m2.id}")
+    expect(merchants["data"][0]["attributes"]["name"]).to eq("#{m1.name}")
+    expect(merchants["data"][1]["attributes"]["name"]).to eq("#{m2.name}")
+  end
+
+  it 'Multi finders - Created At' do
+    m1 = create(:merchant, name: "M1", created_at: "2012-03-27 14:54:09 UTC", updated_at: "2012-03-27 14:54:09 UTC")
+    m2 = create(:merchant, name: "M2", created_at: "2012-03-27 14:54:09 UTC", updated_at: "2012-04-27 14:54:09 UTC")
+    m3 = create(:merchant, name: "M3", created_at: "2012-04-27 14:54:09 UTC", updated_at: "2012-04-27 14:54:09 UTC")
+
+    get "/api/v1/merchants/find_all?created_at=#{m1.created_at}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants["data"].count).to eq(2)
+    expect(merchants["data"][0]["id"]).to eq("#{m1.id}")
+    expect(merchants["data"][1]["id"]).to eq("#{m2.id}")
+    expect(merchants["data"][0]["attributes"]["name"]).to eq("#{m1.name}")
+    expect(merchants["data"][1]["attributes"]["name"]).to eq("#{m2.name}")
+  end
+
+  it 'Multi finders - Updated At' do
+    m1 = create(:merchant, name: "M1", created_at: "2012-03-27 14:54:09 UTC", updated_at: "2012-03-27 14:54:09 UTC")
+    m2 = create(:merchant, name: "M2", created_at: "2012-04-27 14:54:09 UTC", updated_at: "2012-03-27 14:54:09 UTC")
+    m3 = create(:merchant, name: "M3", created_at: "2012-04-27 14:54:09 UTC", updated_at: "2012-04-27 14:54:09 UTC")
+
+    get "/api/v1/merchants/find_all?updated_at=#{m1.updated_at}"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants["data"].count).to eq(2)
+    expect(merchants["data"][0]["id"]).to eq("#{m1.id}")
+    expect(merchants["data"][1]["id"]).to eq("#{m2.id}")
+    expect(merchants["data"][0]["attributes"]["name"]).to eq("#{m1.name}")
+    expect(merchants["data"][1]["attributes"]["name"]).to eq("#{m2.name}")
+  end
+
 end
