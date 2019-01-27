b84b19f67ff625d7b0ae969e6318571548b877f6
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-27 10:50:22 -0700

    Passes Multi Finder - ID

--- app/controllers/api/v1/transactions/search_controller.rb
+++ app/controllers/api/v1/transactions/search_controller.rb
@@ -4,8 +4,13 @@ class Api::V1::Transactions::SearchController < ApplicationController
     render json: TransactionSerializer.new(Transaction.find_by(look_up_params))
   end
 
+  def index
+    render json: TransactionSerializer.new(Transaction.where(look_up_params))
+  end
+
   private
     def look_up_params
       params.permit(:id, :invoice_id, :credit_card_number, :result, :created_at, :updated_at)
     end
+
 end
