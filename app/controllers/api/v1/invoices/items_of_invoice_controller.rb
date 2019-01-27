2ef795cea0747bb259188abcc683bab86296944c
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-27 16:43:31 -0700

    Passes - Returns a collection of associated items

--- app/controllers/api/v1/invoices/items_of_invoice_controller.rb
+++ app/controllers/api/v1/invoices/items_of_invoice_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Invoices::ItemsOfInvoiceController < ApplicationController
 
-  def index
-    render json: ItemSerializer.new(InvoiceItem.where(Invoice.find_by(look_up_params)).items)
+  def index    
+    render json: ItemSerializer.new((Invoice.find_by(look_up_params)).find_items)
   end
 
   private
