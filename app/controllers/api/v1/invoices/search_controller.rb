2867ae366f1f167baa58f654da70522a45dc4b4d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-26 21:50:42 -0700

    Adds testing for multi-finders for invoies

--- app/controllers/api/v1/invoices/search_controller.rb
+++ app/controllers/api/v1/invoices/search_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::Invoices::SearchController < ApplicationController
     render json: InvoiceSerializer.new(Invoice.find_by(look_up_params))
   end
 
+  def index
+    render json: InvoiceSerializer.new(Invoice.where(look_up_params))
+  end
+
   private
     def look_up_params
       params.permit(:id, :customer_id, :merchant_id, :status, :created_at, :updated_at)
--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -91,6 +91,7 @@ describe "Invoices API - Single Finders" do
 
     invoices = JSON.parse(response.body)
 
+    expect(invoices.count).to eq(1)
     expect(invoices["data"]["id"]).to eq(i2.id.to_s)
     expect(invoices["data"]["attributes"]["customer_id"]).to eq(c2.id)
   end
@@ -133,6 +134,135 @@ describe "Invoices API - Single Finders" do
 
     expect(invoices["data"]["id"]).to eq(i1.id.to_s)
     expect(invoices["data"]["attributes"]["customer_id"]).to eq(c1.id)
+  end
+
+end
+
+describe "Invoices API - Multi Finders" do
+  it 'Multi Finder - ID' do
+    c1 = create(:customer)
+    c2 = create(:customer)
+
+    m1 = create(:merchant)
+    m2 = create(:merchant)
+
+    i1 = create(:invoice, customer: c1, merchant: m1, status: "shipped", created_at: "2012-03-13 16:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+    i2 = create(:invoice, customer: c2, merchant: m2, status: "shipped", created_at: "2013-03-13 16:54:10 UTC", updated_at: "2013-03-07 12:54:10 UTC")
+
+    get "/api/v1/invoices/find_all?id=#{i1.id}"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"].count).to eq(1)
+    expect(invoices["data"][0]["id"]).to eq(i1.id.to_s)
+    expect(invoices["data"][0]["attributes"]["merchant_id"]).to eq(m1.id)
+  end
+
+  it 'Multi Finder - Customer_Id' do
+    c1 = create(:customer)
+    c2 = create(:customer)
+
+    m1 = create(:merchant)
+    m2 = create(:merchant)
+
+    i1 = create(:invoice, customer: c1, merchant: m1, status: "shipped", created_at: "2012-03-13 16:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+    i2 = create(:invoice, customer: c2, merchant: m2, status: "shipped", created_at: "2013-03-13 16:54:10 UTC", updated_at: "2013-03-07 12:54:10 UTC")
+
+    get "/api/v1/invoices/find_all?customer_id=#{c1.id}"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"].count).to eq(2)
+    expect(invoices["data"][0]["id"]).to eq(i1.id.to_s)
+    expect(invoices["data"][0]["attributes"]["merchant_id"]).to eq(m1.id)
+  end
+
+  it 'Multi Finder - Merchant_Id' do
+    c1 = create(:customer)
+    c2 = create(:customer)
+
+    m1 = create(:merchant)
+    m2 = create(:merchant)
+
+    i1 = create(:invoice, customer: c1, merchant: m1, status: "shipped", created_at: "2012-03-13 16:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+    i2 = create(:invoice, customer: c2, merchant: m2, status: "shipped", created_at: "2013-03-13 16:54:10 UTC", updated_at: "2013-03-07 12:54:10 UTC")
+
+    get "/api/v1/invoices/find_all?merchant_id=#{m1.id}"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"].count).to eq(2)
+    expect(invoices["data"][0]["id"]).to eq(i1.id.to_s)
+    expect(invoices["data"][0]["attributes"]["customer_id"]).to eq(c1.id)
+  end
+
+  it 'Multi Finder - Status' do
+    c1 = create(:customer)
+    c2 = create(:customer)
+
+    m1 = create(:merchant)
+    m2 = create(:merchant)
+
+    i1 = create(:invoice, customer: c1, merchant: m1, status: "not shipped", created_at: "2012-03-13 16:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+    i2 = create(:invoice, customer: c2, merchant: m2, status: "shipped", created_at: "2013-03-13 16:54:10 UTC", updated_at: "2013-03-07 12:54:10 UTC")
+
+    get "/api/v1/invoices/find_all?status=shipped"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices.count).to eq(1)
+    expect(invoices["data"][0]["id"]).to eq(i2.id.to_s)
+    expect(invoices["data"][0]["attributes"]["customer_id"]).to eq(c2.id)
+  end
+
+  it 'Multi Finder - Created_At' do
+    c1 = create(:customer)
+    c2 = create(:customer)
+
+    m1 = create(:merchant)
+    m2 = create(:merchant)
+
+    i1 = create(:invoice, customer: c1, merchant: m1, status: "not shipped", created_at: "2012-03-13 16:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+    i2 = create(:invoice, customer: c2, merchant: m2, status: "shipped", created_at: "2013-03-13 16:54:10 UTC", updated_at: "2013-03-07 12:54:10 UTC")
+
+    get "/api/v1/invoices/find_all?created_at=#{i1.created_at}"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"].count).to eq(2)
+    expect(invoices["data"][0]["id"]).to eq(i1.id.to_s)
+    expect(invoices["data"][0]["attributes"]["customer_id"]).to eq(c1.id)
+  end
+
+  it 'Multi Finder - Updated_At' do
+    c1 = create(:customer)
+    c2 = create(:customer)
+
+    m1 = create(:merchant)
+    m2 = create(:merchant)
+
+    i1 = create(:invoice, customer: c1, merchant: m1, status: "not shipped", created_at: "2012-03-13 16:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+    i2 = create(:invoice, customer: c2, merchant: m2, status: "shipped", created_at: "2013-03-13 16:54:10 UTC", updated_at: "2013-03-07 12:54:10 UTC")
+
+    get "/api/v1/invoices/find_all?updated_at=#{i1.updated_at}"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"].count).to eq(2)
+    expect(invoices["data"][0]["id"]).to eq(i1.id.to_s)
+    expect(invoices["data"][0]["attributes"]["customer_id"]).to eq(c1.id)
   end
 
 end
