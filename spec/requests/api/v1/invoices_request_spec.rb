58e18f4d263b79b0741bfd43e5a742d6d1858986
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-01-26 21:00:15 -0700

    Passes customer_id search for invoice

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -32,5 +32,26 @@ describe "Invoices API - Single Finders" do
     invoices = JSON.parse(response.body)
 
     expect(invoices["data"]["id"]).to eq(i1.id.to_s)
+    expect(invoices["data"]["attributes"]["merchant_id"]).to eq(m1.id)
+  end
+
+  it 'Single Finder - Customer_Id' do
+    c1 = create(:customer)
+    c2 = create(:customer)
+
+    m1 = create(:merchant)
+    m2 = create(:merchant)
+
+    i1 = create(:invoice, customer: c1, merchant: m1, status: "shipped", created_at: "2012-03-13 16:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+    i2 = create(:invoice, customer: c2, merchant: m2, status: "shipped", created_at: "2013-03-13 16:54:10 UTC", updated_at: "2013-03-07 12:54:10 UTC")
+
+    get "/api/v1/invoices/find?customer_id=#{c1.id}"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"]["id"]).to eq(i1.id.to_s)
+    expect(invoices["data"]["attributes"]["merchant_id"]).to eq(m1.id)
   end
 end
